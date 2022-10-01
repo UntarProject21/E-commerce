d8c9779fc8e5a03608325c0fb8ddbed8dec7be4c
William-535210013 <william.535210013@stu.untar.ac.id>
2022-10-01 11:32:54 +0700

    ...

--- js/FAQ.js
+++ js/FAQ.js
@@ -1,3 +1,11 @@
+const hamburer = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+if (hamburer) {
+    hamburer.addEventListener("click", () => {
+    navList.classList.toggle("open");
+    });
+}
 const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach((faq) =>{
