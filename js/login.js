ae9c40fa1fd1ee7869f83ef20ebb9bc9639748b9
William-535210013 <william.535210013@stu.untar.ac.id>
2022-10-01 21:22:07 +0700

    login done

--- js/login.js
+++ js/login.js
@@ -6,3 +6,54 @@ if (hamburer) {
     navList.classList.toggle("open");
   });
 }
+
+// Dummy login
+
+let usernameInput = document.getElementById('email');
+let passwordInput = document.getElementById('password');
+let loginButton = document.getElementById('loginButton');
+let logoutButton = document.getElementById('logoutButton');
+let formLog = document.getElementById('loginForm');
+let welcome = document.getElementById('adminOnly');
+
+formLog.style.display = "block";
+welcome.style.display = "none";
+logoutButton.style.display = "none";
+
+function onLogin() {
+  localStorage.setItem("username", usernameInput.value);
+  logoutButton.style.display = "block";
+  if(usernameInput.value == "admin" && passwordInput.value == "admin123") {
+    localStorage.setItem("role", "admin");
+    usernameInput.style.display = "none";
+    passwordInput.style.display = "none";
+    loginButton.style.display = "none";
+    formLog.style.display = "none";
+    welcome.style.display = "block";
+  }else {
+    localStorage.setItem("role", "basic");
+    usernameInput.style.display = "none";
+    passwordInput.style.display = "none";
+    loginButton.style.display = "none";
+    formLog.style.display = "none";
+  }
+}
+
+if(localStorage.getItem('username')){
+  usernameInput.style.display = "none"
+  passwordInput.style.display = "none"
+  loginButton.style.display = "none"
+  logoutButton.style.display = "block";
+  if(localStorage.getItem('role') == "admin"){
+    formLog.style.display = "none";
+    welcome.style.display = "block";
+  }else{
+    formLog.style.display = "none";
+    
+  }
+}
+
+function onLogout() {
+  localStorage.clear();
+  location.reload();
+}
