26738c5e077c6baadf6188ceef6ce340e0e4b984
felixfren <felix.535210052@stu.untar.ac.id>
2022-10-05 22:25:53 +0700

    shipping

--- js/shipping.js
+++ js/shipping.js
@@ -6,10 +6,3 @@ if (hamburer) {
     navList.classList.toggle("open");
     });
 }
-const faqs = document.querySelectorAll(".faq");
-
-faqs.forEach((faq) =>{
-    faq.addEventListener("click", () =>{
-        faq.classList.toggle("active");
-    });
-});
