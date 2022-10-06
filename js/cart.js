472b5b922fac6f4e48ce4f955d04833db16367aa
felixfren <felix.535210052@stu.untar.ac.id>
2022-10-06 19:54:24 +0700

    cart

--- js/cart.js
+++ js/cart.js
@@ -24,7 +24,7 @@ function ready() {
         button.addEventListener('click', addToCartClicked)
     }
 
-    document.getElementsByClassName('checkout btn')[0].addEventListener('click', purchaseClicked)
+    //document.getElementsByClassName('checkout btn')[0].addEventListener('click', purchaseClicked)
 
 }
 
@@ -157,3 +157,28 @@ function updateCartTotal() {
 <a class="checkout btn">Proceed To Checkout</a>`
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("checkout");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
