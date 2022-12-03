047c514df418d4c2fd3118b04e4e551af974b970
felixfren <felix.535210052@stu.untar.ac.id>
2022-12-03 15:29:40 +0700

    product final

--- public/js/productDetails.js
+++ public/js/productDetails.js
@@ -1,1 +1,20 @@
+// var e = document.getElementById("sizes");
+// var value = e.value;
+// var text = e.options[e.selectedIndex].text;
+// var stock = document.getElementsByClassName("stock");
 
+// e.onchange = updateStock;
+
+// function updateStock() {
+// 	console.log(1923912831298391);
+// 	if (value == "0") {
+// 		stock.text = 21;
+// 		stock.innerHTML = 21;
+// 	} else if (value == "1") {
+// 		stock.innerHTML = 21;
+// 	} else if (value == "2") {
+// 		stock.innerHTML = 21;
+// 	}
+// }
+
+// module.exports = updateStock;
