c3a422e0300057ecf8fbeee1d5939b714232d9e5
felixfren <felix.535210052@stu.untar.ac.id>
2022-12-04 11:35:14 +0700

    cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,81 +1,84 @@
-const { localsName } = require("ejs");
+// if (document.readyState == 'loading') {
+//     document.addEventListener('DOMContentLoaded', ready)
+// } else {
+//     ready()
+// }
 
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
-} else {
-    ready()
+function view_cart() {
+    console.log(12314123)
+    var bag = JSON.parse(localStorage.getItem("cart"));
+    let total_price = 0;
+    var html = '';
+
+    for (var i = 0; i < bag.length; i++) {
+        console.log(i);
+        let request = new XMLHttpRequest();
+        request.open('GET', `api/getProduct?id=${bag[i].product_id}`, false);
+        request.send(null);
+
+        if (request.status === 200) {
+            let data = JSON.parse(request.responseText);
+            total_price += parseInt(data[0].PRICE * bag[i].quantity);
+            html += `
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-7 col-md-6 col-lg-4">
+                            <img alt="placeholder image" class="img-fluid lazyload" src="img/${data[0].IMAGE}"
+                                width="180px" height="180px" />
+                        </div>
+                        <div class="col-sm-5 col-md-6 col-lg-8">
+                            <h2>${data[0].TITLE}</h2>
+                            <h4>Size : ${data[0].SIZE}</h4>
+                            <h6>Qty</h6>
+                            <div style="display: flex;">
+                                <span class="input-group-btn">
+                                    <button type="button" class="btn btn-default btn-number" style="color: white;"
+                                        onclick="minus('${bag[i].product_id}', '${data[0].PRICE}')"><i class="far fa-minus-square"></i></button>
+                                </span>
+                                <input class="box" type="text" disabled="disabled" value="${bag[i].quantity}"
+                                    class="form-control input-number" id="${bag[i].product_id}" />
+                                <span class="input-group-btn">
+                                    <button type="button" class="btn btn-default btn-number" style="color: white;"
+                                        onclick="add('${bag[i].product_id}', '${data[0].PRICE}')"><i class="far fa-plus-square"></i></button>
+                                </span>
+                            </div>
+                            <br>
+                            <p id="${bag[i].product_id}-price" class="price">Rp ${data[0].PRICE * bag[i].quantity}</p>
+                            <a href="javascript:;" onclick='return remove("${bag[i].product_id}")' style="color:red;"><i class="far fa-trash-alt" color="red"></i> <u>Remove</u></a>
+                            <br><br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+        }
+    }
+    document.getElementById("view-cart").innerHTML = html;
+    document.getElementById("total-price").innerHTML = 'Rp ' + total_price;
 }
 
-function ready() {
-  var cart = JSON.parse(localStorage.getItem("cart"));
-  let total_price = 0;
-  var html = '';
-
-  for (var i = 0; i < cart.length; i++) {
-      //let request = new XMLHttpRequest();
-      //request.open('GET', `api/getProduct?id=${cart[i].product_id}`, false); DATABASE OK
-      //request.send(null);
-
-      //if (request.status === 200) {
-          let data = JSON.parse(request.responseText);
-          total_price += parseInt(data[0].PRICE * cart[i].quantity);
-          html += `
-          <div class="card">
-              <div class="card-body">
-                  <div class="row">
-                      <div class="col-sm-7 col-md-6 col-lg-4">
-                          <img alt="placeholder image" class="img-fluid lazyload" src="img/${data[0].IMAGE}"
-                              width="180px" height="180px" />
-                      </div>
-                      <div class="col-sm-5 col-md-6 col-lg-8">
-                          <h2>${data[0].TITLE}</h2>
-                          <h4>Size : ${data[0].SIZE}</h4>
-                          <h6>Qty</h6>
-                          <div style="display: flex;">
-                              <span class="input-group-btn">
-                                  <button type="button" class="btn btn-default btn-number" style="color: white;"
-                                      onclick="minus('${cart[i].product_id}', '${data[0].PRICE}')"><i class="far fa-minus-square"></i></button>
-                              </span>
-                              <input class="box" type="text" disabled="disabled" value="${cart[i].quantity}"
-                                  class="form-control input-number" id="${cart[i].product_id}" />
-                              <span class="input-group-btn">
-                                  <button type="button" class="btn btn-default btn-number" style="color: white;"
-                                      onclick="add('${cart[i].product_id}', '${data[0].PRICE}')"><i class="far fa-plus-square"></i></button>
-                              </span>
-                          </div>
-                          <br>
-                          <p id="${cart[i].product_id}-price" class="price">Rp ${data[0].PRICE * cart[i].quantity}</p>
-                          <a href="javascript:;" onclick='return remove("${cart[i].product_id}")' style="color:red;"><i class="far fa-trash-alt" color="red"></i> <u>Remove</u></a>
-                          <br><br>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          `;
-      }
-  //}
-  document.getElementById("view-cart").innerHTML = html;
-  document.getElementById("total-price").innerHTML = 'Rp ' + total_price;
-  // updateCartTotal()
-    // var removeCartItemButtons = document.getElementsByClassName('cart-delete')
-    // for (var i = 0; i < removeCartItemButtons.length; i++) {
-    //     var button = removeCartItemButtons[i]
-    //     button.addEventListener('click', removeCartItem)
-    // }
-
-    // var quantityInputs = document.getElementsByClassName('cart-buttons')
-    // for (var i = 0; i < quantityInputs.length; i++) {
-    //     var input = quantityInputs[i]
-    //     input.addEventListener('change', quantityChanged)
-    // }
-
-    // var addToCartButtons = document.getElementsByClassName('bx bx-heart')
-    // for (var i = 0; i < addToCartButtons.length; i++) {
-    //     var button = addToCartButtons[i]
-    //     button.addEventListener('click', addToCartClicked)
-    // }
-
-    // document.getElementsByClassName('checkout btn')[0].addEventListener('click', showModal)
+function addToCart(product) {
+    if (localStorage.getItem("cart") === null) {
+        localStorage.setItem("cart", "[]");
+    }
+    var cart = JSON.parse(localStorage.getItem("cart"));
+    var filtered_cart = cart.filter( x => x.product == product);
+    if (filtered_cart.length === 0) {
+        cart.push({
+            product: product,
+            quantity: 1
+        });
+    } else {
+        filtered_cart[0].quantity++;
+        cart = cart.filter( x => {
+            if (x.product != product) {
+                return x;
+            }
+       });
+        cart.push(filtered_cart[0]);
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 function purchaseClicked() {
@@ -95,28 +98,6 @@ function removeCartItem(event) {
   document.getElementsByClassName('checkout btn')[0].addEventListener('click', showModal)
 }
 
-function addToCart(product) {
-     if (localStorage.getItem("cart") === null) {
-         localStorage.setItem("cart", "[]");
-     }
-     var cart = JSON.parse(localStorage.getItem("cart"));
-     var filtered_cart = cart.filter( x => x.product == product);
-     if (filtered_cart.length === 0) {
-         cart.push({
-             product: product,
-             quantity: 1
-         });
-     } else {
-         filtered_cart[0].quantity++;
-         cart = cart.filter( x => {
-             if (x.product != product) {
-                 return x;
-             }
-        });
-         cart.push(filtered_cart[0]);
-     }
-     localStorage.setItem("cart", JSON.stringify(cart));
-}
 
 
 function addToCartClicked(event) {
@@ -163,7 +144,7 @@ function addItemToCart(title, price, imageSrc) {
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
     cartRow.getElementsByClassName('bx bx-trash')[0].addEventListener('click', removeCartItem)
-	  cartRow.getElementsByClassName('bx bx-cart')[0].addEventListener('click', removeCartItem)
+	cartRow.getElementsByClassName('bx bx-cart')[0].addEventListener('click', removeCartItem)
     cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
 }
 
@@ -284,26 +265,27 @@ function checkoutTotal() {
 } 
 
 // Get the modal
-var modal = document.getElementById("myModal");
-var contentmodal = document.getElementsByClassName("modal-content");
+// var modal = document.getElementById("myModal");
+// var contentmodal = document.getElementsByClassName("modal-content");
 
-// When the user clicks on the button, open the modal
-function showModal() {
-  document.getElementsByClassName('close')[0].addEventListener('click', closeModal)
-  document.getElementsByClassName('submit-btn')[0].addEventListener('click', purchaseClicked)
-  modal.style.display = "block";
-  checkoutItems();
-  checkoutTotal();
-}
+// // When the user clicks on the button, open the modal
+// function showModal() {
+//   document.getElementsByClassName('close')[0].addEventListener('click', closeModal)
+//   document.getElementsByClassName('submit-btn')[0].addEventListener('click', purchaseClicked)
+//   modal.style.display = "block";
+//   checkoutItems();
+//   checkoutTotal();
+// }
 
-function closeModal() {
-  var cartItemContainer = document.getElementsByTagName('table')[2]
-  for(var i = 0;i<cartItemContainer.rows.length;){
-    cartItemContainer.deleteRow(i);
-  }
-  modal.style.display = "none";
+// function closeModal() {
+//   var cartItemContainer = document.getElementsByTagName('table')[2]
+//   for(var i = 0;i<cartItemContainer.rows.length;){
+//     cartItemContainer.deleteRow(i);
+//   }
+//   modal.style.display = "none";
 
-}
+// }
 
+view_cart();
 
-module.exports = addToCart
+module.exports = addToCart;
